e0203b0c74fd97dae1e9c14f4b2aadb02231c99f
ssumin6 <mfni4a14>
2017-11-19 16:07:23 +0900

    initial commit

---
+++ app/src/main/java/com/network/user/a20160333_proj5/MainActivity.java
@@ -0,0 +1,13 @@
+package com.network.user.a20160333_proj5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
